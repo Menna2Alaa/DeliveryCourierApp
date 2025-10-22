d0449aa760ff396da37b864d3812da0fa67d3943
Menna Alaa <menna1722002@gmail.com>
2025-10-23 00:02:19 +0300

    add get_it package create get_it_service class

---
+++ lib/core/services/get_it_service.dart
@@ -0,0 +1,14 @@
+import 'package:delivery_courier_app/core/services/firebase_auth_service.dart';
+import 'package:delivery_courier_app/features/auth/data/repos/auth_repo_impl.dart';
+import 'package:delivery_courier_app/features/auth/domain/repos/auth_repo.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setupGetIt() {
+  getIt.registerSingleton<FirebaseAuthService>(FirebaseAuthService());
+
+  getIt.registerSingleton<AuthRepo>(
+    AuthRepoImpl(firebaseAuthService: getIt<FirebaseAuthService>()),
+  );
+}
--- lib/features/auth/presentation/views/sign_up_view.dart
+++ lib/features/auth/presentation/views/sign_up_view.dart
@@ -1,5 +1,9 @@
+import 'package:delivery_courier_app/core/services/get_it_service.dart';
+import 'package:delivery_courier_app/features/auth/domain/repos/auth_repo.dart';
+import 'package:delivery_courier_app/features/auth/presentation/cubits/sign_up_cubit/sign_up_cubit.dart';
 import 'package:delivery_courier_app/features/auth/presentation/views/widgets/sign_up_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignUpView extends StatelessWidget {
   const SignUpView({super.key});
@@ -7,6 +11,9 @@ class SignUpView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: const SafeArea(child: SignUpViewBody()));
+    return BlocProvider(
+      create: (BuildContext context) => SignupCubit(getIt<AuthRepo>()),
+      child: Scaffold(body: const SafeArea(child: SignUpViewBody())),
+    );
   }
 }
