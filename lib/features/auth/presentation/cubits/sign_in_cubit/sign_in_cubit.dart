d26d7e94fe4d8f5c69cac5cded98da9f706b6a19
Menna Alaa <menna1722002@gmail.com>
2025-10-23 19:09:34 +0300

    create sign in cubit

---
+++ lib/features/auth/presentation/cubits/sign_in_cubit/sign_in_cubit.dart
@@ -0,0 +1,18 @@
+import 'package:delivery_courier_app/features/auth/domain/entities/user_entity.dart';
+import 'package:delivery_courier_app/features/auth/domain/repos/auth_repo.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+part 'sign_in_state.dart';
+
+class SignInCubit extends Cubit<SignInState> {
+  SignInCubit(this.authRepo) : super(SignInInitial());
+
+  final AuthRepo authRepo;
+  Future<void> signInWithEmailAndPassword(String email, String password) async {
+    emit(SignInLoading());
+    var result = await authRepo.signinWithEmailAndPassword(email, password);
+    result.fold(
+      (failure) => emit(SignInFailure(message: failure.message)),
+      (userEntity) => emit(SignInSuccess(userEntity: userEntity)),
+    );
+  }
+}
--- lib/features/auth/presentation/views/sign_in_view.dart
+++ lib/features/auth/presentation/views/sign_in_view.dart
@@ -1,5 +1,9 @@
+import 'package:delivery_courier_app/core/services/get_it_service.dart';
+import 'package:delivery_courier_app/features/auth/domain/repos/auth_repo.dart';
+import 'package:delivery_courier_app/features/auth/presentation/cubits/sign_in_cubit/sign_in_cubit.dart';
 import 'package:delivery_courier_app/features/auth/presentation/views/widgets/sign_in_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignInView extends StatelessWidget {
   const SignInView({super.key});
@@ -7,6 +11,9 @@ class SignInView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SafeArea(child: const SignInViewBody()));
+    return BlocProvider(
+      create: (BuildContext context) => SignInCubit(getIt<AuthRepo>()),
+      child: Scaffold(body: SafeArea(child: const SignInViewBody())),
+    );
   }
 }
