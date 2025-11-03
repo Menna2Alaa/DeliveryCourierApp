41b83d2615f5d328636ca3fa481f09c2a4d43474
Menna Alaa <menna1722002@gmail.com>
2025-11-03 23:26:55 +0200

    fix the problem ProviderNotFoundException (Error: Could not find the correct Provider<GetPackagesCubit> above this BlocBuilder<GetPackagesCubit, GetPackageState> Widget when move from sign in to home

--- lib/features/auth/presentation/views/widgets/sign_in_view_body_bloc_consumer.dart
+++ lib/features/auth/presentation/views/widgets/sign_in_view_body_bloc_consumer.dart
@@ -2,7 +2,7 @@ import 'package:delivery_courier_app/core/helper_function/build_error_bar.dart';
 import 'package:delivery_courier_app/core/widgets/custome_progress_hud.dart';
 import 'package:delivery_courier_app/features/auth/presentation/cubits/sign_in_cubit/sign_in_cubit.dart';
 import 'package:delivery_courier_app/features/auth/presentation/views/widgets/sign_in_view_body.dart';
-import 'package:delivery_courier_app/features/home/presentation/views/home_view.dart';
+import 'package:delivery_courier_app/features/splash/presentation/views/widgets/main_layout.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -20,7 +20,7 @@ class SignInviewBodyBlocConsumer extends StatelessWidget {
       },
       listener: (BuildContext context, state) {
         if (state is SignInSuccess) {
-          Navigator.pushNamed(context, HomeView.routeName);
+          Navigator.pushReplacementNamed(context, MainLayout.routeName);
         }
         if (state is SignInFailure) {
           buildErrorBar(context, state.message);
